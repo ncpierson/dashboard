b0b03ab37e6bd7974507a91c76f9cd811accbbea
Nick Pierson <me@nick.exposed>
2018-09-22 22:37:31 +0000

    initial commit; create-react-app barebones

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+const App = () => {
+  return (
+    <main>
+      <section id="header">
+        nick, exposed
+      </section>
+    </main>
+  );
+};
+
+export default App;
