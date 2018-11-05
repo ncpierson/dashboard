872e3ecdee9059a84d602508593f29ad9df9e923
Nick Pierson <me@nick.exposed>
2018-11-05 03:32:47 +0000

    remove schedule

--- src/Widgets/Widgets.js
+++ src/Widgets/Widgets.js
@@ -13,14 +13,11 @@ const Budget = <Widget icon="💰" slug="budget" key="budget" />;
 const Goals = <Widget icon="🥇" slug="goals" key="goals" />;
 const Portfolio = <Widget icon="📁" slug="portfolio" key="portfolio" />;
 const Resume = <Widget icon="📜" slug="resume" key="resume" />;
-const Schedule = <Widget icon="🕰️" slug="schedule" key="schedule" />;
 const Todo = <Widget icon="☑️" slug="todo" key="todo" />;
 
 const Widgets = () => {
   return (
-    <StyledList>
-      {[Blog, Budget, Goals, Portfolio, Resume, Schedule, Todo]}
-    </StyledList>
+    <StyledList>{[Blog, Budget, Goals, Portfolio, Resume, Todo]}</StyledList>
   );
 };
 
